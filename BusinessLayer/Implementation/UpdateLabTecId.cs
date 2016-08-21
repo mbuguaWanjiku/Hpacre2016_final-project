07e96010ba8f95c0d3d5e93d538fd6dad4bf81a8
esw2015 <marcia_95_cost@hotmail.com>
2016-08-22 00:18:06 +0100

    c.string

--- BusinessLayer/Implementation/UpdateLabTecId.cs
+++ BusinessLayer/Implementation/UpdateLabTecId.cs
@@ -6,22 +6,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessLayer.Implementation {
-    public class UpdateLabTecId {
+namespace BusinessLayer.Implementation
+{
+    public class UpdateLabTecId
+    {
 
-        public void AccessDatabase(int currentUser, int idManager) {
-            //using(SqlConnection connection = new SqlConnection("Data Source=SQL5025.myASP.NET;Initial Catalog=DB_A0ADFA_HPCareDBContext;User Id=DB_A0ADFA_HPCareDBContext_admin;Password=hpcare2016;")) {
-            //using(SqlConnection connection = new SqlConnection("Data Source=MÁRCIA\\SQLSERVER; Initial Catalog =HPCareDBContext; Integrated Security=true")) {
-            using (SqlConnection connection = new SqlConnection("Data Source= WANJIKU\\NEWSQLEXPRESS ; Initial Catalog =HPCareDBContext;Integrated Security=SSPI"))
+        public void AccessDatabase(int currentUser, int idManager)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=SQL5025.myASP.NET;Initial Catalog=DB_A0ADFA_HPCareDBContext;User Id=DB_A0ADFA_HPCareDBContext_admin;Password=hpcare2016;"))
             {
-                SqlCommand command = new SqlCommand("update MCDTStaffManagers set Staff_User_id = " + currentUser + " where MCDTStaffManager_id = " + idManager + ";", connection);
+                //using(SqlConnection connection = new SqlConnection("Data Source=MÁRCIA\\SQLSERVER; Initial Catalog =HPCareDBContext; Integrated Security=true")) {
+                //    using (SqlConnection connection = new SqlConnection("Data Source= WANJIKU\\NEWSQLEXPRESS ; Initial Catalog =HPCareDBContext;Integrated Security=SSPI"))
+                {
+                    SqlCommand command = new SqlCommand("update MCDTStaffManagers set Staff_User_id = " + currentUser + " where MCDTStaffManager_id = " + idManager + ";", connection);
 
-                command.CommandType = CommandType.Text;
-                command.Connection = connection;
-                connection.Open();
-                command.ExecuteNonQuery();
+                    command.CommandType = CommandType.Text;
+                    command.Connection = connection;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+
             }
-
         }
     }
 }
