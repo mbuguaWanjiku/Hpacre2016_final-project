bb680b96c4b82e2abaf392744c13bfb30926d829
esw2015 <marcia_95_cost@hotmail.com>
2016-08-22 00:16:27 +0100

    c.string

--- BusinessLayer/Implementation/ImpHome.cs
+++ BusinessLayer/Implementation/ImpHome.cs
@@ -12,8 +12,8 @@ namespace BusinessLayer.Implementation {
     public class ImpHome {
 
         public Users AccessDatabase(string PatientToSearch) {
-           // using(SqlConnection connection = new SqlConnection("Data Source=SQL5025.myASP.NET;Initial Catalog=DB_A0ADFA_HPCareDBContext;User Id=DB_A0ADFA_HPCareDBContext_admin;Password=hpcare2016;")) {
-                using(SqlConnection connection = new SqlConnection("Data Source= WANJIKU\\NEWSQLEXPRESS ; Initial Catalog =HPCareDBContext;Integrated Security=SSPI")) {
+           using(SqlConnection connection = new SqlConnection("Data Source=SQL5025.myASP.NET;Initial Catalog=DB_A0ADFA_HPCareDBContext;User Id=DB_A0ADFA_HPCareDBContext_admin;Password=hpcare2016;")) {
+             //   using(SqlConnection connection = new SqlConnection("Data Source= WANJIKU\\NEWSQLEXPRESS ; Initial Catalog =HPCareDBContext;Integrated Security=SSPI")) {
 
                 SqlCommand command = new SqlCommand("select * from Users where user_identification = '" + PatientToSearch + "';", connection);
 
