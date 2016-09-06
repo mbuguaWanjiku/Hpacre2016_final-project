b2c08f7bf2194b76581f2c10352005cbbd1272f1
pfc2016 <hpcare2016>
2016-09-06 05:32:09 +0100

    singleton null solved

--- BusinessLayer/SingletonClinicRegistry.cs
+++ BusinessLayer/SingletonClinicRegistry.cs
@@ -32,11 +32,12 @@ namespace BusinessLayer
             CurrentUserId currentStaff = new CurrentUserId();
             int currentIdStaff = currentStaff.AccessDatabase(HttpContext.Current.User.Identity.Name);
             //Users staff = currentStaff.ReturnCurrentUser(currentIdStaff);
-            Users staff = context.Users.Find(currentIdStaff) as Staff;         
+            Staff staff = context.Users.Find(currentIdStaff) as Staff;         
                 if (singletonInstance == null)
                 {
-                    singletonInstance = new ClinicRegistryManager { ClinicRegistryManagerId = AccessDatabase(patient, staff, context).ClinicRegistryManagerId };
-                    Visit visit = new Visit { Visit_Date = DateTime.Today.Date,Visit_Hour = DateTime.Now.TimeOfDay};
+                   // singletonInstance = new ClinicRegistryManager { ClinicRegistryManagerId = AccessDatabase(patient, staff, context).ClinicRegistryManagerId };
+                singletonInstance = new ClinicRegistryManager { Clinic_patient = patient ,Staff_doctor = staff };
+                Visit visit = new Visit { Visit_Date = DateTime.Today.Date,Visit_Hour = DateTime.Now.TimeOfDay};
                     context.VisitManagers.Add(new VisitManager { visit = visit, PatientVisitRegistry = singletonInstance });
                     context.SaveChanges();
                 }
@@ -49,18 +50,18 @@ namespace BusinessLayer
         /// </summary>
         /// <returns></returns>
      
-        private static ClinicRegistryManager AccessDatabase(Patient patient, Users staff, HPCareDBContext context)
-        {
-            using (SqlConnection connection = new SqlConnection("Data Source=SQL5025.myASP.NET;Initial Catalog=DB_A0ADFA_HPCareDBContext;User Id=DB_A0ADFA_HPCareDBContext_admin;Password=hpcare2016;"))
-            {
-                SqlCommand command = new SqlCommand("insert into ClinicRegistryManagers (Clinic_patient_User_id, Staff_doctor_User_id) values ( " + patient.User_id + ", " + staff.User_id + "); select scope_identity();", connection);
-                command.CommandType = CommandType.Text;
-                command.Connection = connection;
-                connection.Open();
-                int id = System.Convert.ToInt32(command.ExecuteScalar());
-                return new ClinicRegistryManager { ClinicRegistryManagerId = id };
-            }
+        //private static ClinicRegistryManager AccessDatabase(Patient patient, Users staff, HPCareDBContext context)
+        //{
+        //    using (SqlConnection connection = new SqlConnection("Data Source=SQL5025.myASP.NET;Initial Catalog=DB_A0ADFA_HPCareDBContext;User Id=DB_A0ADFA_HPCareDBContext_admin;Password=hpcare2016;"))
+        //    {
+        //        SqlCommand command = new SqlCommand("insert into ClinicRegistryManagers (Clinic_patient_User_id, Staff_doctor_User_id) values ( " + patient.User_id + ", " + staff.User_id + "); select scope_identity();", connection);
+        //        command.CommandType = CommandType.Text;
+        //        command.Connection = connection;
+        //        connection.Open();
+        //        int id = System.Convert.ToInt32(command.ExecuteScalar());
+        //        return new ClinicRegistryManager { ClinicRegistryManagerId = id };
+        //    }
 
-        }
+        //}
     }
 }
