927a411336c4549a4521b4cfbf369e760bd444f6
pfc2016 <hpcare2016>
2016-09-19 21:37:51 +0100

    help

--- HPCareNovaVersao/Content/Angular/controllers/HelpController.js
+++ HPCareNovaVersao/Content/Angular/controllers/HelpController.js
@@ -1,15 +1,15 @@
 ﻿app.controller("HelpController", function ($scope) {
-    $scope.helpSearchPatient = 'Code to insert: Unique identification of a  patient. Press Search in the end to proceed the search.';
+    $scope.helpSearchPatient = 'insert userID: UserID is a Unique patient identification number. Press Search to proceed.';
 
-    $scope.helpClassifyDisease = 'Create a new diagnosis for a patient: Insert the category then the sub-category. Press Save To Database to confirm the creation of the new diagnosis.'
+    $scope.helpClassifyDisease = 'Create a new diagnosis for a patient: Choose the category then the sub-category. Press Save To Database to confirm the  diagnosis.'
 
-    $scope.helpGetRegularExamsHistory = 'Query the MCDTs results prescribed by the clinic. Text: Query the results through text. Graph: Query the results of each MCDT component through graph. Control Line Graph: Query the results of a specific component in a MCDT providing the max and min values';
+    $scope.helpGetRegularExamsHistory = 'visualize the MCDTs results prescribed by the clinician. Text: visualize the results by text. Evolution Graph: Visualize the results of evolution of each MCDT component . Control Line Graph:Visualize the results of a specific exam component against control values';
 
     $scope.helpPrescribeMedicationHistory = 'Query the medication history of a patient.';
 
-    $scope.helpPrescribeMedication = 'Prescribe a new medication for a patient. Choose the category, subcategory, dosage, frequency, administration, start date and end date. Press Add To Pool to temporarily hold the prescrition. Press Save To Database to save the new medication prescrition.';
+    $scope.helpPrescribeMedication = 'Prescribe a new medication . Choose the category, subcategory, dosage, frequency, administration, start date and end date. Press Add To Pool to temporarily hold the prescrition. Press Save To Database to save the new prescrition.';
 
-    $scope.helpListMdts = 'Insert the Lab Results. Press + to only see the lab exams from the chosen patient. Press the icon to add the results of that lab exam. To save the results, press Save.';
+    $scope.helpListMdts = 'Insert the Lab Results. Press + to  visualize the lab exams  of the selected patient. Press the icon to add the results of that lab exam. To save the results, press Save.';
 
     $scope.helpUpdateDiseaseStatus = 'To Update a disease status, choose a disease and press Deactivate. The disease will be deadtivated immediately.';
 
